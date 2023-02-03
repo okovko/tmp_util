87d86098cb27d791b9e6cf294ff58b7128142bd4
oleg <okovko@gmail.com>
2023-02-03 01:13:28 -0600

    created integer_sequence_util.hpp, added make_reverse_integer_sequence

---
+++ integer_sequence_util.hpp
@@ -0,0 +1,19 @@
+#ifndef INTEGER_SEQUENCE_UTIL_H_
+#define INTEGER_SEQUENCE_UTIL_H_
+
+#include <utility>
+#include <cstddef>
+
+/*
+ * partial template specialization is necessary to retrieve the nested pack in the template param
+*/
+template <std::size_t, typename>
+struct make_reverse_index_sequence_helper;
+
+template <std::size_t N, std::size_t... Is>
+struct make_reverse_index_sequence_helper<N, std::index_sequence<Is...>> : std::index_sequence<(N - Is - 1)...> {};
+
+template <size_t N>
+struct make_reverse_index_sequence : make_reverse_index_sequence_helper<N, decltype(std::make_index_sequence<N>{})> {};
+
+#endif // INTEGER_SEQUENCE_UTIL_H_
