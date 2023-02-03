81b993712c4d56ae28ebbabbf2cb9018d64488a3
oleg <okovko@gmail.com>
2023-02-03 01:50:52 -0600

    just auto type the tuple in example_integer_sequence_util.cpp

--- example_integer_sequence_util.cpp
+++ example_integer_sequence_util.cpp
@@ -4,7 +4,7 @@
 
 int main() {
   std::tuple tup{1, 2.0, true};
-  std::tuple rev_tup = [&tup]<std::size_t... Is>(std::index_sequence<Is...>) {
+  auto rev_tup = [&tup]<std::size_t... Is>(std::index_sequence<Is...>) {
     return std::tuple{std::get<Is>(tup)...};
   }(make_reverse_index_sequence<std::tuple_size_v<decltype(tup)>>{});
   std::apply([](const auto&... x) {
